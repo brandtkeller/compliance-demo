d4d31cdfbe916bf87770404c498d214204a89c5e
Brandt Keller <brandt.keller@defenseunicorns.com>
2023-11-03 21:19:35 -0700

    create bundle file for 0.0.5 cli

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd bundle && uds bundle create --set INIT_VERSION=v0.29.2 --set METALLB_VERSION=0.0.1 --set DUBBD_VERSION=0.11.0 --set IDAM_VERSION=0.1.12 --set SSO_VERSION=0.1.7 --confirm
